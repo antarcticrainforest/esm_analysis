fbacd376c34f6d87a5393787f2e0d23ecd1040e2
antarcticrainforest <martin.bergemann@posteo.org>
2019-09-25 17:36:28 +0200

    Update conf to make readthedocs work

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -10,9 +11,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+ import os
+ import sys
+ sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -21,9 +22,13 @@ project = 'esm-analysis'
 copyright = '2019, Martin Bergemann'
 author = 'Martin Bergemann'
 
+# For :ghissue: and :ghpull: links.
+github_project_url = "https://github.com/antarcticrainforest/esm_analysis/"
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
+# The master toctree document.
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
@@ -31,7 +36,13 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'recommonmark'
+        'recommonmark',
+        'sphinx.ext.autodoc',
+        'sphinx.ext.intersphinx',
+        'sphinx.ext.napoleon',
+        'nbsphinx',
+        'sphinxcontrib_github_alt',
+
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -40,7 +51,14 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
+language = None
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -48,7 +66,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
